6937c1185b89c81f2bc8470da862d76fc7a8a6a2
Dakota States <dakota.states@gmail.com>
2020-02-12 15:35:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each {|name| badges << "Hello, my name is #{name}."}
+  badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index {|name,index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  room_assignments
+end
+
+def printer(attendees)
+badges_and_room_assignments = [batch_badge_creator(attendees), assign_rooms(attendees)]
+badges_and_room_assignments.each {|x| puts x}
+end
